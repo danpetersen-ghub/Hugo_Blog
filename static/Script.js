53af968f646e9fd35db2d3c291c47ba8bba14ad3
Dan <danpetersen@hotmail.co.uk>
2022-03-12 11:49:23 +0000

    Nav Across All Pages

--- static/Script.js
+++ static/Script.js
@@ -4,34 +4,36 @@ let close = document.getElementsByClassName("nav-close")[0];
 let nav_li = document.getElementsByClassName("nav-li");
 
 hamburger.addEventListener("click", function () {
-  console.log("ham clicked");
+  console.log("Mobile Nav - Expand Clicked");
   close.style.display = "block";
   hamburger.innerHTML = ``;
-  showHideNavelinks("show", nav_li);
+  showHideNodeList("show", nav_li);
 });
 
 close.addEventListener("click", function () {
-  console.log("close clicked");
+  console.log("Mobile Nav - Close Clicked");
   hamburger.innerHTML = `<i class="fa-solid fa-bars"></i>`;
   close.style.display = "none";
-  showHideNavelinks("hide", nav_li);
+  showHideNodeList("hide", nav_li);
 });
 
-function showHideNavelinks(command, nodelist) {
+function showHideNodeList(command, nodelist) {
   if (command === "show") {
+    console.log("Mobile - Show");
     for (var i = 0; i < nodelist.length; i++) {
       nodelist[i].style.display = "block";
     }
   }
   if (command === "hide") {
+    console.log("Mobile - Hide");
     for (var i = 0; i < nodelist.length; i++) {
       nodelist[i].style.display = "none";
     }
   }
 }
-// if (this.innerWidth < 600) {
-//   showHideNavelinks("hide", nav_li);
-// }
+if (window.innerWidth < 600) {
+  showHideNodeList("hide", nav_li);
+}
 
 // document.addEventListener("change", function () {
 //   if (this.innerWidth < 600) {
