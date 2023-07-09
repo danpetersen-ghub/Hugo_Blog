5b1b988ea793fd9f23c1315ca2680ef3731b87d8
Dan <danpetersen@hotmail.co.uk>
2023-07-09 11:05:44 +0100

    tina Config

--- tina/config.js
+++ tina/config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from "tinacms";
 import { post_templateFields } from "./templates";
 
+import { config as dotenvConfig } from 'dotenv';
+import { defineConfig } from 'some-library'; // Replace 'some-library' with the actual library you are using
+
+// Load the environment variables from the .env file
+dotenvConfig();
+
+// Retrieve the values from the environment variables
+const clientId = process.env.CLIENT_ID || '<your-tina-cloud-projects-client-id>';
+const token = process.env.TOKEN || '<your-tina-cloud-projects-token>';
+
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId, // Get this from tina.io
+  token, // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: "admin",
